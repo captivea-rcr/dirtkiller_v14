6ac3fe8bb6d0b952fa1faee49a28e08bcb0907f4
Ronald Crump <76955120+captivea-rcr@users.noreply.github.com>
2021-08-26 00:54:39 -0400

    Delete generate_dynamic_cheque_app directory

--- generate_dynamic_cheque_app/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import print_dynamic_cheque_report_wizard
-from . import print_dynamic_cheque_report_action
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
